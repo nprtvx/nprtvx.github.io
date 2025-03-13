62e79bc84ca45eafccf5f23b5878b9011cfe6430
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-03-14 01:38:36 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/Home.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('neon-monkey-popeye-1269-232629')
+);
