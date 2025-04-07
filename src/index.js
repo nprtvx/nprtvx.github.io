5ce197aed298d5af76e9d5184bf78dbb56d29ad2
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-04-07 19:00:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +13,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 );
