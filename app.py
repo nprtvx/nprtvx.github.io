5423455cc5d42f1d6d974049681bd70c9b3cb7e4
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-04-07 21:07:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask
 from flask_frozen import Freezer
 
 app = Flask(__name__)
+app.config['FREEZER_DESTINATION'] = 'build'
 
 @app.route('/')
 def index():
     return '<h2>ello world</h2>'
 
+@app.route('/about')
+def about():
+    return 'About Page'
+
 freezer = Freezer(app)
 
+@freezer.register_generator
+def url_generator():
+    yield '/'
+    yield '/about'
+
 if __name__ == '__main__':
-    freezer.freeze()
+    if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
+        freezer.freeze()
+    else:
+        app.run(debug=True)
