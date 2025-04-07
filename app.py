9ce0bee62cb6f01b03d40e7c9b0006b903d4fdb0
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-04-07 21:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_frozen import Freezer
 
 app = Flask(__name__)
-freezer = Freezer(app)
 
 @app.route('/')
 def index():
     return '<h2>ello world</h2>'
+
+freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
