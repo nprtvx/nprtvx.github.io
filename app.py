0b351086fa439a8d70485107cda66e49efe661c5
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-04-07 20:46:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    return render_template('./public/index.html')
+
+if __name__ == '__main__':
+    freezer.freeze()
