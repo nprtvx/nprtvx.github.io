1b0350e3754556f4e9b6f89c75c4d8bb69553700
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-06-20 21:12:39 +0530

    Update popeye.py

--- src/popeye.py
+++ src/popeye.py
@@ -1,15 +1,29 @@
 from src.BaseClassesForNM import Style, Page
 
 # css styles
-css = ""
+css = """
+   div {
+      display: flex;
+      font-size: 48px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #26119226;
+      border-radius: 26px;
+      box-shadow: 5px 10px 18px #66b88b;
+      padding: 2em;
+   }
+"""
 
 # get style for popeye page
 style = Style(extra_css=css)
 
 # body for popeye page
-body = "
-   ello mastaru
-"
+body = """
+   <div>
+      nah! nothing here
+   </div>
+"""
 # create page for popeye
 page = Page(
     title="Popeye",
