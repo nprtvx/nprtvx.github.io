5326b670492926a7af78854ae5cac1bbafb36e65
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-06-20 23:06:17 +0530

    Create home.py

---
+++ src/home.py
@@ -0,0 +1,32 @@
+from src.BaseClassesForNM import Page, Style
+
+css = f"""
+    h1 {{
+        text-transform: uppercase;
+    }}
+    .landing-poster {{
+        height: 120px;
+        margin: 26px;
+        padding: 26px;
+        background: url("./landing-poster.jpg");
+    }}
+    #landing-poster-heading {{
+        font-size: 48px;
+        color: #261192;
+    }}
+"""
+style = Style(extra_css=css)
+
+body = f"""
+    <section class="landing-poster">
+        <h1 id="landing-poster-heading">welcome to neon monkey</h1>
+    </section>
+"""
+
+page = Page(
+    title="Home",
+    body=body,
+    links=[("About", "/about/"), ("Contact", "/contact/")],
+    style=style,
+    filename="index.html"
+)
