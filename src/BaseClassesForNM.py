51c979c37ae84a14576dc3c5ddf60b327600de77
nprtvx <neonmonkey08@gmail.com>
2025-06-22 13:00:26 +0530

    bug-fix

--- src/BaseClassesForNM.py
+++ src/BaseClassesForNM.py
@@ -93,7 +93,7 @@ class Style:
         """
 
 class Page:
-    def __init__(self, title, logo=None, body, links=None, style=None, filename="page.html"):
+    def __init__(self, title, body, links=None, style=None, filename="page.html", logo=None):
         self.title = title
         self.logo = logo
         self.body = body
