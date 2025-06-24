30b0a13919845c0252767745bae4a90fc3785449
nprtvx <neonmonkey08@gmail.com>
2025-06-25 01:30:33 +0530

    blacknwhite

--- src/blackNwhite.py
+++ src/blackNwhite.py
@@ -12,5 +12,34 @@ page.body = page.body + """ <div id="animated-bouncing-ball">
 		width: 25px; height: 25px; background-color: #26119287; 
 		animation: bounce 8s infinite;
 	}
-
+	@keyframes bounce {
+		0% {
+			background-color: #000000a8;
+			width: 26px;
+			height: 26px;
+			border-radius: 26px 99px 36px 56px;
+			box-shadow: 0 0 8px #a2a2a2a2 inset;
+		}
+		25% {
+			background-color: #ff0000a8;
+			width: 48px;
+			height: 48px;
+			border-radius: 26px 99px 36px 56px;
+			box-shadow: 0 0 8px #a2a2a2a2 inset;
+		}
+		50% {
+			background-color: #00ff00a8;
+			width: 69px;
+			height: 69px;
+			border-radius: 26px 99px 36px 56px;
+			box-shadow: 0 0 8px #a2a2a2a2 inset;
+		}
+		100% {
+			background-color: #0000ffa8;
+			width: 87px;
+			height: 87px;
+			border-radius: 26px 99px 36px 56px;
+			box-shadow: 0 0 8px #a2a2a2a2 inset;
+		}
+	}
 """
