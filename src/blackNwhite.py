0fcb7c22f280aaba98343cf418a73fec9dce105d
nprtvx <neonmonkey08@gmail.com>
2025-06-25 01:35:34 +0530

    blacknwhite

--- src/blackNwhite.py
+++ src/blackNwhite.py
@@ -5,7 +5,7 @@ page = Page( title="Popeye", body=HTML_BODY, links=None, style=style,
     filename="popeye/index.html"
 )
 
-page.body = page.body + ''.join(page.body, "<div id='animated-bouncing-ball'>animated bouncing ball</div> "
+page.body = page.body + ''.join(page.body, "<div id='animated-bouncing-ball'>animated bouncing ball</div> ")
 page.style.extra_css = page.style.extra_css + """
 	#animated-bouncing-ball {
 		width: 25px; height: 25px; background-color: #26119287; 
