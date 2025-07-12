63e509673e1e031751f57a903426dc89c18f2dee
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-12 12:19:33 +0530

    Update maw.py

--- src/maw.py
+++ src/maw.py
@@ -1,53 +1,33 @@
 from src.BaseClassesForNM import Page, Style
 
 css = f"""
-	.landing-poster {{
-	        margin: 2em;
-        	padding: 2em 0;
-		border-radius: 2em;
-	        box-shadow: 0 0 1em #29eaea inset;
-	}}
-
-	#landing-poster-heading {{
-		font-size: xxx-large;
-		color: #3692;
-		text-shadow: 0 0 8px #abcdef;
-		letter-spacing: 18px;
-		padding: 0 2em;
-		text-align: center;
-	}}
-
-	.card-div {{
-		background-color: #29eaea87;
-		padding: 1px 0;
-		margin: 0 8em;
-	}}
-    #maw-div {{
-        color: #29e;
-        background-color: #000d;
-        font-size: 12rem;
-        height: 100vmin;
+    #profile {{
         display: flex;
         aligin-items: center;
         justify-content: center;
+								flex-direction: column;
+								padding: 1rem 0;
     }}
+				#fullname {{
+				    color: #29e;
+        background-color: #000d;
+        font-size: 8rem;
+								margin: 1rem 0;
+			 }}
+				#email {{
+				    color: #29e;
+        background-color: #000d;
+        font-size: 8rem;
+								margin: 1rem 0;
+			 }}
 """
 style = Style(extra_css=css)
 
-landing_poster = f"""
-	<div class="landing-poster">
-	        <h1 id="landing-poster-heading">welcome to neon monkey</h1>
-	</div>
-"""
-
-after_landing_poster = f"""
-	<div id="after-landing-poster">
-		<div class="card-div"></div>
-	</div>
-"""
-
 body = f"""
-    <div id='maw-div'>ello mawduuuu i❤️u</div>
+				<ul id='profile'>
+				<li id='fullname'>tejitha yenupuri</li>
+				<li id='fullname'>ytejita95@gmail.com</li>
+				</ul>
 """
 
 page = Page(
