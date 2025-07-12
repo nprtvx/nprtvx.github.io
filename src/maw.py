9d899123a55a28a83a3cf591fc843be461811bb8
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-12 12:28:46 +0530

    Update maw.py

--- src/maw.py
+++ src/maw.py
@@ -3,7 +3,7 @@ from src.BaseClassesForNM import Page, Style
 css = f"""
     #profile {{
         display: flex;
-        aligin-items: center;
+        align-items: center;
         justify-content: center;
         flex-direction: column;
         padding: 1rem 0;
@@ -24,10 +24,12 @@ css = f"""
 style = Style(extra_css=css)
 
 body = f"""
-				<ul id='profile'>
-				<li id='fullname'>tejitha yenupuri</li>
-				<li id='fullname'>ytejita95@gmail.com</li>
-				</ul>
+    <h2>profile</h2>
+    <p>strong, independent dentist from india working in the usa as a full time lead co-ordinator at "soul dental" west</p>
+    <ul id='profile'>
+        <li id='fullname'>tejitha yenupuri</li>
+        <li id='fullname'>ytejita95@gmail.com</li>
+    </ul>
 """
 
 page = Page(
