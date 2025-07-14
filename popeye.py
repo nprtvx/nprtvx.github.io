eb3a42924f1875c313c2632ea1a97181d741d9e9
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-14 22:55:19 +0530

    Update popeye.py

--- popeye.py
+++ popeye.py
@@ -3,3 +3,12 @@
 
 title = "nMe of yge app".title()
 
+class Page:
+    def render(elements: {}, css_styles: {}):
+        return elements,css_styles
+    def get_elements(self):
+        for element in self.elements
+            if element:
+                pass
+def defaults():
+    return self.baseHtml or BaseHtml()
