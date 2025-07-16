3bfdb5a4d626df55e95d6616a6721d54343580ff
nprtvx <nprtvx@gmail.com>
2025-07-17 03:51:39 +0530

    -nothing

--- src/page.py
+++ src/page.py
@@ -22,36 +22,10 @@ class Page:
 		<head>
 			<meta charset="UTF-8">
 			<title>{title +" | "+ self.title if self.title != "Home" else str('neon monkey'.title())}</title>
-			<link rel="icon" href="/assets/favicon.ico">
-			<style>
-				@import url('https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double:wght@100..900&family=Permanent+Marker&display=swap');
-				{self.style.render()}
-			</style>
 		</head>
         """
         self.body_html = f"""
-		<body>
-			<div id='whole-package'>
-				<div id="navbar">
-					<a class="logo" id="logo" href="/">
-						<svg xmlns="https://www.w3.org/2000/svg/" viewBox="0 0 100 100" width="87px" id="logo-svg-nm">
-							<polygon points="16,92 8,52 8,8 26,8 50,50 68,8 84,8 92,8 92,52 84,92 50,50 16,16 16,92" fill="#8926"></polygon>
-							<polygon points="50,50 76,16 84,16 84,52 76,68 50,50" fill="#9006"></polygon>
-						</svg>
-						{title}
-					</a>
-					<div class="menu" id="menu-lines">
-						<span id="menu-line"></span>
-						<span id="menu-line"></span>
-						<span id="menu-line"></span>
-					</div>
-				</div>
-				{self.body}
-				<div id='copy-right'>
-					&copy; {title} {datetime.datetime.now().year}
-				</div>
-			</div>
-		</body>
+		<body></body>
 	"""
 
         return f"""
