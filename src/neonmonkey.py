659c2ca32951819315ff2b1812f022c887fdbdd2
nprtvx <nprtvx@gmail.com>
2025-07-16 13:56:34 +0530

    add-heading-title

--- src/neonmonkey.py
+++ src/neonmonkey.py
@@ -5,4 +5,11 @@ title = str("neonmonkey").title()
 
 title = title.strip('')
 
+for letter in title:
+	if letter.lower() in ['n', 'm', 'e']:
+		TITLE += letter.upper()
+	elif letter == 'o':
+		TITLE += '0'
+	else:
+
 title = ' '.join(title)
