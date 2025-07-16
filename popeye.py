458b1cfc7bd648b9b7e60ccb42d664279b8e3569
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-17 00:13:31 +0530

    Update popeye.py

--- popeye.py
+++ popeye.py
@@ -31,7 +31,7 @@ if __name__=="__main__":
         if not page.logo:
             page.logo = Path('assets/logo.png') if page.title == "Home" else Path('../assets/logo.png')
         """write page"""
-        page.body = popeye
+        page.body = body
         page.write()
 
 #EoF
