247c11ccd3eebb4f29862ec6d5772068f24c2137
nprtvx <nprtvx@gmail.com>
2025-07-16 17:59:05 +0530

    add-heading-title

--- src/BaseClassesForNM.py
+++ src/BaseClassesForNM.py
@@ -90,7 +90,9 @@ class Page:
         self.filename = filename
 
     def render(self):
-        title = str('neon monkey'.title())
+        title = str('ne0NM0NkEy'.title())
+	title = title.strip()
+	title = ' '.join(title)
         links_html = " | ".join(
             f'<a href="{href}">{text}</a>' for text, href in self.links
         )
@@ -109,18 +111,18 @@ class Page:
         self.body_html = f"""
 		<body>
 			<div id='whole-package'>
-				<div id='navbar'>
-					<a class='logo' id='logo' href='/'>
-						<svg xmlns="https://www.w3.org/2000/svg/" viewBox="0 0 100 100" width="87px" id='logo-svg-nm'>
-							<polygon points="16,92 8,52 8,8 26,8 50,50 68,8 84,8 92,8 92,52 84,92 50,50 16,16 16,92" fill="#892"></polygon>
-							<polygon points="50,50 76,16 84,16 84,52 76,68 50,50" fill="#900"></polygon>
+				<div id="navbar">
+					<a class="logo" id="logo" href="/">
+						<svg xmlns="https://www.w3.org/2000/svg/" viewBox="0 0 100 100" width="39px" id="logo-svg-nm">
+							<polygon points="16,92 8,52 8,8 26,8 50,50 68,8 84,8 92,8 92,52 84,92 50,50 16,16 16,92" fill="#8926"></polygon>
+							<polygon points="50,50 76,16 84,16 84,52 76,68 50,50" fill="#9006"></polygon>
 						</svg>
 						{title}
 					</a>
-					<div class='menu' id='menu-lines'>
-						<span id='menu-line'></span>
-						<span id='menu-line'></span>
-						<span id='menu-line'></span>
+					<div class="menu" id="menu-lines">
+						<span id="menu-line"></span>
+						<span id="menu-line"></span>
+						<span id="menu-line"></span>
 					</div>
 				</div>
 				{self.body}
