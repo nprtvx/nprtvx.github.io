ee108b3ab77e10a95ef764233b64de9e3bd9b4cd
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-17 04:28:08 +0530

    Update popeye.py

--- src/popeye.py
+++ src/popeye.py
@@ -1,13 +1,14 @@
 from src.page import Page
 from src.style import Style
-
+print('popeye in src after imports')
 style = Style()
 
 vr0000m = " vroooom ".capitalize();
+print('popeye page ', vr0000m)
 
 body = f"""
     <div id="vroom">{vr0000m}</div>
 """
+print(body)
 
 page = Page(title="popeye", body=body, style=style, filename='index.html')
-
