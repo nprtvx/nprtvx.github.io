c339fb11864a833eab2cab1c7dad34c5795789ec
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-16 23:10:56 +0530

    Update popeye.py

--- popeye.py
+++ popeye.py
@@ -2,11 +2,21 @@
 
 
 ## ## 👾👾👾👾
+
+# ## ###### import home
+from src.home import page as home
+#from src.about import page as about
+#from src.contact import page as contact
+#from src.popeye import page as popeye
+#from src.a404 import page as a404
+#from src.blackNwhite import  page as blackNwhite
+#from src import cars
+
 # ## ######
 body = f"""
     <div id="popeye"></div>
 """
 
-
+# ## ######
 popeye = body
 
