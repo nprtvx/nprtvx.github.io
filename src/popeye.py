cebb1aca06ff50ad326024115ba80c1765b2e038
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-16 22:01:57 +0530

    Update popeye.py

--- src/popeye.py
+++ src/popeye.py
@@ -1,77 +1,10 @@
-from src.page import Page
-from src.style import Style
 
-# CSS styles for the Popeye page
-CSS_STYLES = """
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
-    }
 
-    @keyframes something {
-        0% {
-            opacity: 0;
-        }
-        20% {
-            opacity: 1;
-        }
-        40% {
-            opacity: 0.4;
-        }
-        60% {
-            opacity: 1;
-        }
-        80% {
-            opacity: 0.8;
-        }
-        100% {
-            opacity: 1;
-        }
-    }
 
-    @keyframes bounce {
-        0%, 20%, 50%, 80%, 100% {
-            transform: translateY(0);
-        }
-        40% {
-            transform: translateY(-30px);
-        }
-        60% {
-            transform: translateY(-15px);
-        }
-	90% {
-            transform: translateY(-20px);
-    }
-    
-    #intro {
-        font-size: 26px;
-        padding-left: 2em;
-    }
+from src import home
+vr0000m = " vroooom ".capitalize();
+
+body = f"""
+    <div id="vroom">{vr0000m}</div>
 """
-
-# HTML body for the Popeye page
-HTML_BODY = """
-    <div id='intro'>
-        popeye the sailor man
-    </div>
-    <div id='something-interesting'>
-        let me know if you got anything interesting
-	<input type='text' id='in-something-interesting' />
-    </div>
-"""
-
-# Style object for the Popeye page
-style = Style(extra_css=CSS_STYLES)
-
-# Page object for the Popeye page
-page = Page(
-    title="Popeye",
-    body=HTML_BODY,
-    links=None,
-    style=style,
-    filename="popeye/index.html"
-)
+print(body)
