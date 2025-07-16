5047b1eb013303e8eadf6b2cfe097fa530c66b69
nprtvx <nprtvx@gmail.com>
2025-07-17 04:01:20 +0530

    nothing

--- build.py
+++ build.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             """page logo defaults to None. Change path argument to update/replace"""
             if not page.logo:
                 page.logo = Path('assets/logo.png') if page.title == "Home" else Path('../assets/logo.png')
-        	"""write page"""
+                """write page"""
 	        page.write()
         print("wrote home page")
     except Exception as exc:
