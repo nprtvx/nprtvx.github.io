53da52a890961d83651045be197dd70982b16d02
nprtvx <nprtvx@gmail.com>
2025-07-19 03:20:38 +0530

    bg_change

--- src/popeye.py
+++ src/popeye.py
@@ -1,10 +1,30 @@
 from src.page import Page
 from src.style import Style
-print('popeye in src after imports')
-style = Style()
+#print('popeye in src after imports')
+css = f"""
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        background-color: #000d;
+    }
+    #popeye {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+"""
+style = Style(extra_css=css)
 
 vr0000m = " vroooom ".capitalize();
-print('popeye page ', vr0000m)
+#print('popeye page ', vr0000m)
 
 body = f"""
     <div id="vroom">{vr0000m}</div>
@@ -17,8 +37,10 @@ body = f"""
 	}}
     </style>
 """
-print(body)
+#print(body)
 
 page = Page(title="popeye", body=body, style=style, filename='index.html')
 
 page.body += body
+
+print(page.body)
