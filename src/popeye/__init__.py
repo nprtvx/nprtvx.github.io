4fb9bc95c5d18096f2c8585d54679228603e83ce
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-19 15:15:16 +0530

    Update __init__.py

--- src/popeye/__init__.py
+++ src/popeye/__init__.py
@@ -20,6 +20,27 @@ css = f"""
 		height: 100px;
 		border-radius: 50vmin;
 	}}
+ 	#login, #signup {{
+		width: 400px;
+  		height: 400px;
+    		display: flex;
+      		align-items: center;
+		justify-content: center;
+  	}}
+   	#loginlink, #signuplink {{
+		text-decoration: none;
+  		color: #8926;
+    		background: transparent;
+      		border: 1px solid;
+		border-radius: 1em;
+  		padding: 1em;
+    		margin: 1em;
+    	}}
+     	#greetinglinks {{
+      		display: flex;
+		align-items: center;
+  		justify-content: space-evenly;
+    	}}
 """
 style = Style(extra_css=css)
 
@@ -35,11 +56,22 @@ def create_element(tag: str, id: str):
 scripts = [
 	"const popeye = document.getElementById('popeye');",
 	f"{create_element('div', 'greeting')}",
-	f"{create_element('a', 'login')}",
-	f"{create_element('a', 'signup')}",
-	"greeting.append(login);",
-	"greeting.append(signup);",
+	f"{create_element('a', 'loginlink')}",
+	f"{create_element('a', 'signuplink')}",
+	f"{create_element('form', 'login')}",
+	f"{create_element('form', 'signup')}",
+	f"{create_element('div', 'greetinglinks')}",
+	"greeting.textContent = 'welcome';",
+	"loginlink.textContent = 'login';",
+	"signuplink.textContent = 'sign up';",
+	"loginlink.setAttribute('href', '#login';",
+	"signuplink.setAttribute('href', '#signup';",
+	"greetinglinks.append(login);",
+	"greetinglinks.append(signup);",
+	"greeting.append(greetinglinks);",
 	"popeye.append(greeting);",
+	"loginlink.addEventListener('click', () => {{ popeye.remove(greeting); popeye.append(login); }});",
+	"signuplink.addEventListener('click', () => {{ popeye.remove(greeting); popeye.append(signup); }});",
 	"////popeye.appendChild(vr00m);"
 ]
 
