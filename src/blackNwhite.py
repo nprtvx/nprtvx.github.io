a3772c9cb5c7d763383a9090ae98f872e8782ea3
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-19 22:13:27 +0530

    Update blackNwhite.py

--- src/blackNwhite.py
+++ src/blackNwhite.py
@@ -1,12 +1,32 @@
 from src.page import Page
 from src.style import Style
 
+
+
 body = """
+<div class='bnw'>
+<h1>black & white</h1>
+<div class='hero'>
+<h2>welcome</h2>
+<ul>
+<li><a href='#'></a></li>
+<li><a href='#'></a></li>
+<li><a href='#'></a></li>
+<li><a href='#'></a></li>
+</ul>
+</div>
+</div>
 """
 
+css = """
+
+"""
 style = Style()
 
 # Page object for the Popeye page
 blackNwhite = Page( title="Black & White", body=body, links=None, style=style, 
     filename="blackNwhite/index.html"
 )
+
+bnw = blackNwhite
+
