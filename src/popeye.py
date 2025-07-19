5daf3beb902548fe2cb951325d9bcbab24ce9e1c
nprtvx <nprtvx@gmail.com>
2025-07-19 11:30:43 +0530

    styles_check

--- src/popeye.py
+++ src/popeye.py
@@ -3,39 +3,29 @@ from src.style import Style
 
 
 css = f"""
-    * {{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }}
-    body {{
-        background-color: #000d;
-    }}
-    #popeye {{
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-    }}
-"""
-style = Style(extra_css=css)
-
-vr0000m = " vroooom ".capitalize();
-
-body = f"""
-    <div id="vroom">{vr0000m}</div>
-    <style>
+	#popeye {{
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}}
 	#vroom {{
 		background-color: #8926;
 		width: 100px;
 		height: 100px;
 		border-radius: 50vmin;
 	}}
-    </style>
+"""
+style = Style(extra_css=css)
+
+vr0000m = " vroooom ".upper();
+
+body = f"""
+    <div id="vroom">{vr0000m}</div>
 """
 
 
