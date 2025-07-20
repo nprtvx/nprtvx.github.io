a919f438e53c95a3a24a6d14c2f32f64452485cf
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-21 00:17:23 +0530

    Update doubleoshadows.py

--- src/doubleoshadows.py
+++ src/doubleoshadows.py
@@ -11,8 +11,9 @@ html = f"""
 """
 
 script = f"""<script>
-const {doubleO} = document.getElementById('{doubleO}-8');
-doubleO.textContent = `${{ {doubleO} }}`;
+const doubleO = document.getElementById('{doubleO}-8');
+doubleO.textContent = ` {doubleO} `;
+document.body.append(doubleO*3)
 </script>
 """
 
