d3c4f93ce01246daed3590dfd853a6d2088662e1
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-21 00:38:58 +0530

    Update doubleoshadows.py

--- src/doubleoshadows.py
+++ src/doubleoshadows.py
@@ -25,7 +25,7 @@ animation: shadow-blink 2s infinite;
 position: absolute;
 top: 126px;
 left: 87px;
-padding: 4rem;
+padding: 4rem 8rem;
 display: center;
 align-items: center;
 justify-content: center;
