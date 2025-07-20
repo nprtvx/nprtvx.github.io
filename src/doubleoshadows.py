2cc5af2d6a1646c6cdcb0b38afd444a62582c211
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-20 22:21:16 +0530

    Create doubleoshadows.py

---
+++ src/doubleoshadows.py
@@ -0,0 +1,43 @@
+
+
+
+letterO = 'o'
+doubleO = letterO*2
+
+html = f"""
+<div class='{doubleO}-div}'></div>
+"""
+
+script = f"""
+"""
+
+style = f"""
+.{doubleO}-div {{
+color: #fff8;
+background-color: #000d;
+font-size: 32px;
+animation: shadow-blink 2s infinite;
+}}
+
+@keyframes shadow-blink {{
+.{doubleO}-div {{
+from {{
+text-shadow: 0 0 #fff;
+}}
+to {{
+text-shadow: 0 26px #fff;
+}}
+}}
+}}
+"""
+
+def get_doubleOshadow(html, style, script):
+    page = Page(body=html+script, style=style, filename="doubleOshadow/index.html")
+
+if __name__=="__main__":
+    doubleOshadow = get_doubleOshadow()
+
+
+
+# eof
+# 
