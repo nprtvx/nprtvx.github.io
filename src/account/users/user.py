1b4dc8a932e65bf6a58627d01fdff517896bfff2
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-05 00:48:40 +0530

    Update user.py

--- src/account/users/user.py
+++ src/account/users/user.py
@@ -2,5 +2,9 @@ import requests
 
 username = 'popeye'
 response = requests.post(f'nprtvx.github.io//account//users//{username}',
-    data={"text": username}) if _.status_code == 202 else 'enter username'
+    data={"text": username})
+if response.status_code == 200:
+    print('success')
+else:
+    print('enter username')
 print(response.text)
