ca1c0d059fcfd72dee6632ba524be857166dc119
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-20 18:35:13 +0530

    Update popeye.py

--- popeye.py
+++ popeye.py
@@ -8,7 +8,7 @@
 #from src.about import page as about
 #from src.contact import page as contact
 #from src.popeye import page as popeye
-#from src.a404 import page as a404
+#from src.a404 import page as d404
 #from src.blackNwhite import  page as blackNwhite
 #from src import cars
 #from src.popeye import page as HOME
