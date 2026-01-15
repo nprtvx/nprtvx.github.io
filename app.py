2a93c34dc8037f3ec3b2b5002cf6512bf2969525
nprtvx <nprtvx@gmail.com>
2026-01-15 08:14:47 +0530

    templates and static

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+title = "neon monkey"
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  # data=await read_index()
+  return render_template('index.html', title=title)
 
-async def read_index():
-    with open("index.html", "r") as file:
-        filist = file.readlines()
-        file.close()
-    await filist
-
-filist =  read_index()
+@app.route("/login")
+def login():
+  return render_template('login.html', title='login | '+title)
