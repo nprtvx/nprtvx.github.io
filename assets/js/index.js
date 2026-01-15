21a3d05a22fde51453f83e2ab79a9a0718f32887
nprtvx <nprtvx@users.noreply.github.com>
2026-01-15 02:45:13 +0000

    deploy: 2a93c34dc8037f3ec3b2b5002cf6512bf2969525

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const nprtvx = document.getElementById("nprtvx");
+
+const bouncingInfiniteElement = document.createElement("div");
+
+const bie = bouncingInfiniteElement;
+
+bie.addEventListener("load", () => {
+    bie.style.animation = "";
+});
+
